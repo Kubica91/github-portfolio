6e396afb2f1e74e3264fc2ae5e1c56e85bd67b11
Kubica91 <petran97@seznam.cz>
2026-05-10 15:39:36 +0200

    copy file, vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 /// <reference types="vitest" />
 
 import react from "@vitejs/plugin-react-swc";
+import { copyFileSync } from "fs";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), checker({ typescript: true })],
+    plugins: [
+        react(),
+        checker({ typescript: true }),
+        {
+            name: "spa-fallback",
+            closeBundle() {
+                const dist = resolve(__dirname, "dist");
+                copyFileSync(resolve(dist, "index.html"), resolve(dist, "404.html"));
+            },
+        },
+    ],
     base: "/github-portfolio",
 });
